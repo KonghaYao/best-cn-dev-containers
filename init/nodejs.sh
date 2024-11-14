638e64f10a34d246e33cacf0392fb0da6f2a30b4
3446798488 <3446798488@qq.com>
2024-11-14 02:43:06 +0000

    fix: 修复换行符在 windows 下爆炸问题

--- init/nodejs.sh
+++ init/nodejs.sh
@@ -1,6 +1,6 @@
 curl -fsSL https://deb.nodesource.com/setup_22.x | sh
 apt install -y nodejs
-node -v && npm -v 
+node -v && npm -v
 npm config set registry https://registry.npmmirror.com
-npm install -g pnpm n 
+npm install -g pnpm n
 npm cache clean --force
