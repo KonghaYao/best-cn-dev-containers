4adf64b806d0b07a85f466d958557bfe2ab36fac
江夏尧 <3446798488@qq.com>
2024-11-24 08:50:41 +0800

    feat: 兼容 x86 和 arm64 容器

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 # ubuntu 源要比 debain 快非常多
-FROM ubuntu:22.04
+FROM ubuntu:24.04
 
 WORKDIR /app
 
-COPY init/sources.list /etc/apt/sources.list
 COPY init /init
+RUN sh /init/ubuntu.sh
 RUN sh /init/linux.sh
 RUN sh /init/nodejs.sh
 COPY init/pip.conf /root/.pip/pip.conf
---
+++ init/ubuntu.sh
@@ -0,0 +1,6 @@
+sed -i 's@http://archive.ubuntu.com@http://mirrors.aliyun.com@g' /etc/apt/sources.list
+sed -i 's@http://security.ubuntu.com@http://mirrors.aliyun.com@g' /etc/apt/sources.list
+sed -i 's@http://ports.ubuntu.com@http://mirrors.aliyun.com@g' /etc/apt/sources.list
+sed -i 's@http://archive.ubuntu.com@http://mirrors.aliyun.com@g' /etc/apt/sources.list.d/ubuntu.sources
+sed -i 's@http://security.ubuntu.com@http://mirrors.aliyun.com@g' /etc/apt/sources.list.d/ubuntu.sources
+sed -i 's@http://ports.ubuntu.com@http://mirrors.aliyun.com@g' /etc/apt/sources.list.d/ubuntu.sources
