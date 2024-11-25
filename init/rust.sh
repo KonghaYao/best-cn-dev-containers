3bd68e73ca3781d1880c814dce504e2c03ac35d8
3446798488 <3446798488@qq.com>
2024-11-25 03:26:05 +0000

    fix: 修复 rust 未注册到全局问题

--- init/rust.sh
+++ init/rust.sh
@@ -4,4 +4,5 @@ export RUSTUP_DIST_SERVER="https://rsproxy.cn"
 export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://rsproxy.cn/rustup-init.sh -o rustup-init.sh
 sh rustup-init.sh -y && rm -rf rustup-init.sh
+. /root/.cargo/env
 cargo -v
