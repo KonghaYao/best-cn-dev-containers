4138ac69429978f3a4b8921651a93ee81f068884
江夏尧 <3446798488@qq.com>
2024-11-29 14:03:40 +0000

    feat: 添加测试 docker 容器环境

--- init/rust.sh
+++ init/rust.sh
@@ -1,8 +1,8 @@
 # 安装 rust
-export PATH="/root/.cargo/bin:$PATH"
 export RUSTUP_DIST_SERVER="https://rsproxy.cn"
 export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://rsproxy.cn/rustup-init.sh -o rustup-init.sh
 sh rustup-init.sh -y && rm -rf rustup-init.sh
-. /root/.cargo/env
-cargo -v
+sh /root/.cargo/env
+cargo -V
+bash
